f9c6c645fe5f88769f4186945b4dd36b1106ee7f
Falasefemi2 <femifalase228@gmail.com>
2024-05-24 13:26:45 +0100

    connection to mongodb

--- database/database.go
+++ database/database.go
@@ -1,1 +1,44 @@
 package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+var DB *mongo.Database
+
+func InitDB() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	uri := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("DB_NAME")
+
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatalf("Error connecting to MongoDB: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = Client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Error pinging MongoDB: %v", err)
+	}
+	DB = client.Database(dbName)
+	log.Println("Connected to MongoDB")
+}
